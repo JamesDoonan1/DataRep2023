3779deeb8552c3796326c141125ffe8c1b4bdbaa
James <g00310428@atu.ie>
2023-11-27 22:24:58 +0000

    Sell component created

--- src/App.js
+++ src/App.js
@@ -4,22 +4,24 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
-
+import Sell from './components/sell';
 
 function App() {
   return (
     <BrowserRouter>
     <div className="App">
       <Navbar bg="danger" data-bs-theme="dark">
-        <Container><Navbar.Brand href="/">Navbar</Navbar.Brand>
+        <Container>
+          <Navbar.Brand href="/">Navbar</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/sell">Sell</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
 
       <Routes>
-        <Route path='/' element={<Content></Content>}></Route>
+        <Route path='/sell' element={<Sell></Sell>}></Route>
       </Routes>
     </div>
     </BrowserRouter>
---
+++ src/components/sell.js
@@ -0,0 +1,78 @@
+import { useState } from "react";
+import axios from "axios";
+
+
+function Sell() {
+    const [state, setState] = useState('');
+    const [brand, setBrand] = useState('');
+    const [image, setImage] = useState('');
+    const [price, setPrice] = useState('');
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        console.log("New or Used: " + state +
+            " Piano Brand: " + brand +
+            " Image: " + image + " Price: " + price);
+
+        const piano = {
+            state: state,
+            brand: brand,
+            image: image,
+            price: price
+        };
+
+        axios.post('http://localhost:3000/api/piano', piano)
+            .then()
+            .catch();
+
+    }
+
+    return (
+        <div>
+            <h2>Please enter your Piano details</h2>
+            <form onSubmit={handleSubmit}>
+                <div className="form-group">
+                    <label>Add Piano status. New or Used: </label>
+                    <input type="text"
+                        className="form-control"
+                        value={state}
+                        onChange={(e) => { setState(e.target.value) }}
+                    />
+                </div>
+                <div className="form-group">
+                    <label>Add Book Brand i.e Yamaha: </label>
+                    <input type="text"
+                        className="form-control"
+                        value={brand}
+                        onChange={(e) => { setBrand(e.target.value) }}
+                    />
+                </div>
+                <div className="form-group">
+                    <label>Add Piano Photo </label>
+                    <input type="text"
+                        className="form-control"
+                        value={image}
+                        onChange={(e) => { setImage(e.target.value) }}
+                    />
+                </div>
+
+                <div className="form-group">
+                    <label>Add Piano Price: </label>
+                    <input type="text"
+                        className="form-control"
+                        value={price}
+                        onChange={(e) => { setPrice(e.target.value) }}
+                    />
+                </div>
+                <div>
+                    <input type="submit"
+                    value="Add Piano">
+                        </input>
+                </div>
+            </form>
+        </div>
+    );
+};
+export default Sell;
